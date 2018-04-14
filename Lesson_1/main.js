1b053f812933bd08c565015cf91503dc95d9ce4a
Yaroslav <misteriysmo@gmail.com>
2018-04-14 18:26:42 +0300

    home work with if-else

--- Lesson_1/main.js
+++ Lesson_1/main.js
@@ -2,18 +2,52 @@
 // 2. После цвета спрашивает память и выдает цену телефона
 // 3. В конце выводится картинка и цена
 
-var colour;
-var model;
-var memory;
-var price;
-var photoPlace = '<p><img src="image\\';
-
 alert("Lesson_1");
 
-var setColour = prompt("Entered color");
+var setModel = prompt("Enter model");
+var setColour = prompt("Enter color");
+var setMemory = prompt("Enter memory");
 
-if (setColour === 'black'){
-    document.write(photoPlace + 'apple_black.jpg" class="center-img"/></p>');
-} else {
-    document.write('Not found');
+if (setModel == 'apple' && setColour == 'black' && setMemory == 4){
+    document.write('<p><img src="image/apple_black.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 500$</p>');
+} else if(setModel == 'apple' && setColour == 'black' && setMemory == 8){
+    document.write('<p><img src="image/apple_black.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 600$</p>');
+}else if(setModel == 'apple' && setColour == 'rose' && setMemory == 4) {
+    document.write('<p><img src="image/apple_rose.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 550$</p>');
+}else if(setModel == 'apple' && setColour == 'rose' && setMemory == 8) {
+    document.write('<p><img src="image/apple_rose.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 750$</p>');
+}else if(setModel == 'meizu' && setColour == 'black' && setMemory == 4) {
+    document.write('<p><img src="image/meizu_black.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 300$</p>');
+}else if(setModel == 'meizu' && setColour == 'black' && setMemory == 8) {
+    document.write('<p><img src="image/meizu_black.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 400$</p>');
+}else if(setModel == 'meizu' && setColour == 'blue' && setMemory == 4) {
+    document.write('<p><img src="image/meizu_blue.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 350$</p>');
+}else if(setModel == 'meizu' && setColour == 'blue' && setMemory == 4) {
+    document.write('<p><img src="image/meizu_black.jpg" class="center-img"/></p>' +
+        '<p>Model: ' + setModel + '</p>' +
+        '<p>Memory: ' + setMemory + '</p>' +
+        '<p>Price: 450$</p>');
+}else {
+    alert("Wrong parameters!!!")
 }
